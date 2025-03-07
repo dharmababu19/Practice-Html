ba06bfdaeddc9c67bbe585a2dd7c9eb2613484c0
dharmababu19 <dharmendrakaranam@gmail.com>
2025-03-07 11:36:15 +0530

    HTML practice

---
+++ Table.html/doc.js
@@ -0,0 +1,6 @@
+console.log("Boys what to do what not to do");
+console.log(" one Changer");
+alert("all details must be enter");
+document.write(" once I stepin history repeats"); 
+document.write("If you are bad I am your Dad");
+document.write( 5 * 6 );
